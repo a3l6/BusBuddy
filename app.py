f9c7e6930bafac0645a98e631568436511923891
Emilio Mendoza <ironislife39@gmail.com>
2024-01-13 11:57:40 -0500

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,52 @@
-from flask import Flask
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import db_handler
+from exceptions import UserAlreadyExists
+
+
+load_dotenv()
+
+dh = db_handler.db_handler()
+
 
 app = Flask(__name__)
 
+# Register Errors
+@app.errorhandler(UserAlreadyExists)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+
 @app.route("/")
 def home():
-    return "Hello World"
+    return os.getenv("PASSWORD")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    address = request.form.get("address")
+
+    dh.register(email, password, address)
+
+    return f"{email,name,password,address=}"
+
+@app.route("/admin/register")
+def admin_register():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    address = request.form.get("address")
+
+    dh.register(email, password, address)
+
+    return "Registered?"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db_handler.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+import sqlalchemy.exc
+from exceptions import UserAlreadyExists
+
+class db_handler:
+    @staticmethod
+    def register(self, *args, **kwargs):
+        try: 
+            pass
+        except sqlalchemy.exc.IntegrityError:
+            raise UserAlreadyExists("User already exists!", status_code=422)
