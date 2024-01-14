1f169e8639d81fa9d4d95354da9193f30b96d4bc
Emilio Mendoza <ironislife39@gmail.com>
2024-01-14 02:25:15 -0500

    Added map generation

--- route_handler.py
+++ route_handler.py
@@ -1,36 +1,35 @@
-def get_routes():
-    list1 = [
-        "119 Osler Dr, Dundas, ON L9H 6X4",
-        "101 Osler Drive 108 A, ON L9H 4H4",
-        "7 Don St, Dundas, ON L9H 4N8",
-        "23 Dunning Ct, Dundas, ON L9H 4K8",
-        "5 South Street E, Dundas, ON L9H 4K5",
-        "41 South St W, Dundas, ON L9H 4C4",
-        "94 Main St Unit 86, Dundas, ON L9H 2H1",
-        "86 Main St, Dundas, ON L9H 2P9",
-        "50 Hatt St, Dundas, ON L9H 0A1",
-        "77 Governors Rd, Dundas, ON L9H 7N8"
-    ]
+import Digraphs
+import time
+from geopy.geocoders import GoogleV3
+import os
 
-    list2 = [
-        "50 Governors Road, Dundas, ON L9H 5M3",
-        "56 Governors Rd, Dundas, ON L9H 5G7",
-        "205B Governors Rd, Dundas, ON L9H 3J7",
-        "14 Kemp Dr, Dundas, ON L9H 2M9",
-        "14 Central Park Ave, Dundas, ON L9H 2M6",
-        "297 Mill St, Dundas, ON L9H 2L8",
-        "366 Mill St, Dundas, ON L9H 2M1",
-        "424 MacNab St, Dundas, ON L9H 2L3",
-        "1 Head St unit 5E, Dundas, ON L9H 3H5",
-        "343 King St W, Dundas, ON L9H 1W8"
-    ]
+def get_routes() -> list[str]: 
 
-    list3 = [
-        "11 Woodlawn Ct, Hamilton, ON L9H 7S2",
-        "15 Romar Dr, Dundas, ON L9H 5E2",
-        "7 Westoby Ct, Dundas, ON L9H 7P9",
-        "15 Parkway Pl, Dundas, ON L9H 6K3",
-        "548 Old Dundas Rd, Ancaster, ON L9G 3J4"
-    ]
+    # calling the Nominatim tool and create Nominatim class
+    loc = GoogleV3(api_key=os.getenv("GOOGLE_MAPS_KEY"))
 
-    return list1, list2, list3
+
+    def get_geocode_obj(addr):
+        try:
+            time.sleep(1)
+            geocode = loc.geocode(addr)
+            return geocode.latitude, geocode.longitude
+        except AttributeError:
+            return
+    x = Digraphs.main()
+
+    lists = []
+
+    for i in x:
+        lists.append(list(map(lambda y: y.address.replace(" ", "+"),list(map(lambda x: loc.reverse(f"{x[0]}, {x[1]}"), i)))))
+
+
+    urls = []
+    for l in lists:
+        url = "https://www.google.com/maps/dir/"
+        for addr in l:
+            url += addr + "/"
+        urls.append(url)
+
+    return urls
+    
